88d281ebd4a03e234dc6a78a2a54513356fa9188
Andre Souza Alves <alves.andre.ds@gmail.com>
2020-08-25 22:53:40 -0300

    Desafio #2

--- spec/requests/weapons_spec.rb
+++ spec/requests/weapons_spec.rb
@@ -2,9 +2,59 @@ require 'rails_helper'
 
 RSpec.describe "Weapons", type: :request do
   describe "GET /weapons" do
-    it "works! (now write some real specs)" do
+
+    it "returns success status" do
       get weapons_path
       expect(response).to have_http_status(200)
+    end
+
+    it "the weapon's title and current power are presents" do
+      weapons = create_list(:weapon, 3)
+      get weapons_path
+      weapons.each do |weapon|
+        expect(response.body).to include(weapon.title)
+        expect(response.body).to include("#{weapon.current_power}")
+      end
+    end
+
+  end
+  describe "POST /weapons" do
+    context "when it has valid parameters" do
+      it "creates the weapon with correct attributes" do
+        weapon_attributes = FactoryBot.attributes_for(:weapon)
+        post weapons_path, params: {weapon: weapon_attributes}
+        expect(Weapon.last).to have_attributes(weapon_attributes)
+      end
+    end
+
+    context "when it has no valid parameters" do
+      it "does not create weapon" do
+        expect{
+          post weapons_path, params: {weapon: {weaponName: '', description: '', power_base: '', power_step: '' , weapon_level:''}}
+        }.to_not change(Weapon, :count)
+      end
+    end
+  end
+
+  describe "DELETE /weapons" do
+    it "Destroy the weapon correct" do
+      weapon = create(:weapon)
+      delete weapon_path(weapon)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /weapons" do
+    it "shows the weapon with correct attributes" do
+      weapon = create(:weapon)
+      get weapon_path(weapon), params: {weapon: weapon}
+      expect(response.body).to include(weapon.weaponName)
+      expect(response.body).to include(weapon.description)
+      expect(response.body).to include("#{weapon.power_base}")
+      expect(response.body).to include("#{weapon.power_step}")
+      expect(response.body).to include("#{weapon.weapon_level}")
+      expect(response.body).to include(weapon.title)
+      expect(response.body).to include("#{weapon.current_power}")
     end
   end
 end
